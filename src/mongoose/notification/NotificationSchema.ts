4b7aa75630b65fe2ff5c4f3720c45297f719c257
Dheshan Mohandass <dheshan.mail@gmail.com>
2022-04-12 13:55:45 -0400

    Implemented Notification Schema, Controller and DAO Dheshan and Owen

---
+++ src/mongoose/notification/NotificationSchema.ts
@@ -0,0 +1,16 @@
+/**
+ * @file Implements mongoose schema for Notifications
+ */
+import mongoose from "mongoose";
+
+export const NotificationSchema = new mongoose.Schema({
+    subject_uid: {type: mongoose.Schema.Types.ObjectId, ref: "userModel"},
+    subject_tid: {type: mongoose.Schema.Types.ObjectId, ref: "tuitModel"},
+    subject_type: {type: String, required: true},
+    verb: {type: String, required: true},
+    predicate_uid: {type: mongoose.Schema.Types.ObjectId, ref: "userModel"},
+    predicate_tid: {type: mongoose.Schema.Types.ObjectId, ref: "tuitModel"},
+    predicate_type: {type: String, required: true},
+
+    recipient: {type: mongoose.Schema.Types.ObjectId, ref: "UserModel"},
+}, {collection: "notifications"})
