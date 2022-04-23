6693ad2cb97a22dca92515d64d7e61a95e0dc3af
Dheshan Mohandass <dheshan.mail@gmail.com>
2022-04-23 11:54:12 -0400

    Documented NotificationDaoI and NotificationControllerI
    Dheshan & Owen

--- src/interfaces/notificaation/NotificationControllerI.ts
+++ src/interfaces/notificaation/NotificationControllerI.ts
@@ -4,14 +4,43 @@
  */
 import {Request, Response} from "express";
 
+/**
+ * Defines the use cases the {@link NotificationsController} is to support.
+ */
 export interface NotificationControllerI {
+    /**
+     * Records the a {@link Notification} and sends the record written
+     * to the database as Response.
+     * @param req {Request} - the Request containing the details of the notification to be created.
+     * @param res {Response} - the Response containing record written to the database
+     */
     createNotification(req: Request, res: Response): void;
 
+    /**
+     * Retrieves all {@link Notification}s in the database.
+     * @param req {Request} - the Request with parameter of interest.
+     * @param res {Response} - the Response containing all notifications in the database.
+     */
     findAllNotifications(req: Request, res: Response): void;
 
+    /**
+     * Retrieves all {@link Notification}s for the intended recipient.
+     * @param req {Request} - the Request with the UID of the intended recipient.
+     * @param res {Response} - the Response containing all notifications for the intended recipient.
+     */
     findNotificationsForUser(req: Request, res: Response): void;
 
+    /**
+     * Retrieves a {@link Notification} with the passed in ID.
+     * @param req {Request} - the Request with the IFD of the notification to be retrieved.
+     * @param res {Response} - the Response containing the notification with the passed in ID.
+     */
     findNotificationsById(req: Request, res: Response): void;
 
+    /**
+     * Deletes a {@link Notification} given its id and returns the status of the delete operation.
+     * @param req {Request} - the Request containing the ID of the notification to be deleted.
+     * @param res {Response} - the Response containing the status of the operation
+     */
     deleteNotificationsById(req: Request, res: Response): void;
 }
--- src/interfaces/notificaation/NotificationDaoI.ts
+++ src/interfaces/notificaation/NotificationDaoI.ts
@@ -3,14 +3,41 @@
  */
 import {Notification} from "../../models/Notification";
 
+/**
+ * Defines the CRUD functions {@link NotificationDao} is to support.
+ */
 export interface NotificationDaoI {
+    /**
+     * Creates a {@link Notification} with the passed in data.
+     * @param notification {Notification} - the Notification to be Created.
+     * @return {Promise<object>} - Promise containing the status of the delete operation.
+     */
     createNotification(notification: any): Promise<Notification | null>;
 
+    /**
+     * Returns all the {@link Notification}s in the database.
+     * @return {Promise<Notification[]>} - Promise containing all the Notifications
+     */
     findAllNotifications(): Promise<Notification[]>;
 
+    /**
+     * Returns all the {@link Notification}s with the intended User as the passed in UID
+     * @param uid {string} - the UID of the intended recipient
+     * @return {Promise<Notification[]>} - Promise containing all the Notifications
+     */
     findNotificationsForUser(uid: string): Promise<Notification[]>;
 
+    /**
+     * Returns the {@link Notification} with the passed in ID
+     * @param id {string} - the ID of the notification to be retrieved
+     * @return {Promise<Notification>} - Promise containing the Notification with the passed in ID.
+     */
     findNotificationsById(id: string): Promise<any>;
 
+    /**
+     * Deletes the {@link Notification} with the passed in ID, and returns the result of the delete operation.
+     * @param id {string} - the ID of the notification to be deleted.
+     * @return {Promise<object>} - Promise containing the status of the delete operation.
+     */
     deleteNotificationsById(id: string): Promise<object>;
 }
